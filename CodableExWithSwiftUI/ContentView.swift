8f5d0b67918a2aab739c55a27b729c58dc382db9
Jason Young Lee <dev.jellyworks@gmail.com>
2021-11-27 23:52:49 +0900

    Decoding 수정중

--- CodableExWithSwiftUI/ContentView.swift
+++ CodableExWithSwiftUI/ContentView.swift
@@ -12,6 +12,45 @@ struct ContentView: View {
         Text("Hello, world!")
             .padding()
     }
+    
+    func decode(){
+        let jsonData = """
+            {
+                "count":3,
+                "datas":[
+                    {
+                        "no":1,
+                        "name":"Kim"
+                    },
+                    {
+                        "no":2,
+                        "name":"Kim"
+                    },
+                    {
+                        "no":3
+                    }]
+            }
+            """.data(using: .utf8)
+        
+    }
+    
+    
+    
+}
+
+struct Person:Codable{
+    var num:Int
+    var name:String?
+    enum CodingKeys:String, CodingKey{
+        case num = "no"
+        case name
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        num = try container.decode(Int.self, forKey: .num)
+        name = try container.decodeIfPresent(String.self, forKey: .name)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
