4fef6e38abc42f19268e89c8e2ffbb90e77e467e
Jason Young Lee <dev.jellyworks@gmail.com>
2021-11-27 23:27:57 +0900

    Initial Commit

---
+++ CodableExWithSwiftUI/CodableExWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableExWithSwiftUIApp.swift
+//  CodableExWithSwiftUI
+//
+//  Created by Jason on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct CodableExWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
